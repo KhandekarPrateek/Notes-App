2541189e1ae3abad49da9e0970f502734b46c59c
KhandekarPrateek <prateekkhandekar01@gmail.com>
2023-12-21 17:38:44 +0530

    text truncate

--- src/pages/DashBoard/NoteTab/index.js
+++ src/pages/DashBoard/NoteTab/index.js
@@ -9,25 +9,19 @@ const NoteTab = ({ noteArray, openNoteBody, updateNote, removeNote }) => {
       <Row className="h-100 border-end g-0 vstack gap-0 dashboard-border">
         {noteArray.map((e, index) => {
           const { noteHeader } = e;
-
           return (
             <>
               {noteHeader && (
-                <div
-                  key={index}
-                  className="note-name-row border-bottom dashboard-border header-hover"
-                >
+                <div className="note-name-row border-bottom dashboard-border header-hover">
                   <div className="justify-content-between d-flex ">
-                    {/* <div className="align-items-center d-flex"> */}
                     <h5
-                      className="icon-cursor note-name "
+                      className="icon-cursor note-name text-truncate "
                       onClick={() => {
                         openNoteBody(index);
                       }}
                     >
                       {e.noteHeader}
                     </h5>
-                    {/* </div> */}
                     <div className="justify-content-end d-flex">
                       <GrUpdate
                         className="icon-cursor mx-2"

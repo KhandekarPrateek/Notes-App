57dbc69e739e6e981ff7b6ceca7eec480b1e5a30
KhandekarPrateek <prateekkhandekar01@gmail.com>
2023-12-21 17:33:05 +0530

    the note name  are highlighted on hover

--- src/pages/DashBoard/NoteTab/index.js
+++ src/pages/DashBoard/NoteTab/index.js
@@ -4,36 +4,30 @@ import { GrUpdate } from "react-icons/gr";
 import { Row } from "reactstrap";
 
 const NoteTab = ({ noteArray, openNoteBody, updateNote, removeNote }) => {
-  const [divBg, setDivBg] = useState();
-
-  const noteNameHighlight = () => {
-    // setDivBg();
-    console.log("hellio");
-  };
-
   return (
     <div className="profile-container">
       <Row className="h-100 border-end g-0 vstack gap-0 dashboard-border">
         {noteArray.map((e, index) => {
           const { noteHeader } = e;
+
           return (
             <>
               {noteHeader && (
                 <div
-                  className="note-name-row border-bottom dashboard-border"
-                  onMouseEnter={noteNameHighlight}
+                  key={index}
+                  className="note-name-row border-bottom dashboard-border header-hover"
                 >
-                  <div className="justify-content-between d-flex">
-                    <div className="align-items-center d-flex">
-                      <h5
-                        className="icon-cursor note-name "
-                        onClick={() => {
-                          openNoteBody(index);
-                        }}
-                      >
-                        {e.noteHeader}
-                      </h5>
-                    </div>
+                  <div className="justify-content-between d-flex ">
+                    {/* <div className="align-items-center d-flex"> */}
+                    <h5
+                      className="icon-cursor note-name "
+                      onClick={() => {
+                        openNoteBody(index);
+                      }}
+                    >
+                      {e.noteHeader}
+                    </h5>
+                    {/* </div> */}
                     <div className="justify-content-end d-flex">
                       <GrUpdate
                         className="icon-cursor mx-2"
